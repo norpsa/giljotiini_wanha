97c40f7c420efcd2dd2f58d2b0e61307ab7cd212
Noora Vainio <nooraevainio@gmail.com>
2016-08-18 22:48:59 +0300

    REST add game, list games, get game by id, delete game

--- index.js
+++ index.js
@@ -11,14 +11,62 @@ app.get('/', function(req, res){
 
 app.use('/api/v1', router);
 
-app.get('/foo', function(req, res){
-  res.sendStatus(200);
-});
-
+// CREATE GAME
 router.post('/game/:name', function(req, res){
 	var name = req.params['name'];
-	console.log(name);
+	var id = new Date().valueOf();
+	var newGame = {name : name,
+					id 	: id};
+	games.push(newGame);
 	res.sendStatus(200);
+});
+
+// GET LIST OF GAMES
+router.get('/game', function(req, res){
+	res.json(games);
+});
+
+// GET GAME BY ID
+router.get('/game/:id', function(req, res){
+	var id = req.params['id'];
+
+	var game;
+
+	for(var i = 0; i < games.length; i++){
+		if(games[i]['id'] == id){
+			game = games[i];
+			break;
+		}
+	}
+
+	if(typeof game === 'undefined'){
+		res.status(404).send('Ei oo');
+	} else {
+		res.json(game);
+	}
+	
+});
+
+// DELETE GAME BY ID
+router.delete('/game/:id', function(req, res){
+	var id = req.params['id'];
+
+	var game;
+
+	for(var i = 0; i < games.length; i++){
+		if(games[i]['id'] == id){
+			game = games[i];
+			games.splice(games.indexOf(game), 1);
+			break;
+		}
+	}
+
+	if(typeof game === 'undefined'){
+		res.status(404).send('Ei oo');
+	} else {
+		res.sendStatus(200);
+	}
+	
 });
 
 http.listen(3000, function(){
